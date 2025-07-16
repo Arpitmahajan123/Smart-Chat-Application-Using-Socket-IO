fa6872a3672322cfb70dbb704dd4fadc56c61f2a
Arpit Mahajan <130462117+Arpitmahajan123@users.noreply.github.com>
2025-07-16 19:35:55 +0530

    Update Client File

--- Backend/index.js
+++ Backend/index.js
@@ -1,34 +1,22 @@
-const io = require('socket.io')(8000);
+const io = require('socket.io')(5000)
+
+
+
 const users = {};
 
 
 io.on('connection', socket => {
-
-    
     socket.on('new-user-joined', name => {
-        
         users[socket.id] = name;
+        console.log(`${name} joined the chat`);
         socket.broadcast.emit('user-joined', name);
-
     });
-
 
     socket.on('send', message => {
         socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
     });
 
 
-    socket.on('disconnect', message => {
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id];
-    });
 
 
-
-
-
-
-
-
-    
 });
--- js/client.js
+++ js/client.js
@@ -1,11 +1,14 @@
-const socket = io('http://localhost:8000');
+const socket = io('http://localhost:5000');
 
 const form = document.getElementById('send-message');
 const messageInput = document.getElementById('messageinput');
 const messageContainer = document.querySelector('.container');
 
 const name = prompt("Enter your name to join the chat");
-socket.emit('new-user-joined', name);
+socket.emit('new-user-joined', this.name);
+
+
+
 
 
 
