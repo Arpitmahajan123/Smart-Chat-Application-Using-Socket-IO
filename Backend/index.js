ed4ebce703999cf6994548e5b509c4077a5bca25
Arpit Mahajan <130462117+Arpitmahajan123@users.noreply.github.com>
2025-07-18 23:53:02 +0530

    Update Client

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,17 @@
-const io = require('socket.io')(5000)
-
-
-
+const io = require('socket.io')(8000);
 const users = {};
 
 
 io.on('connection', socket => {
     socket.on('new-user-joined', name => {
         users[socket.id] = name;
-        console.log(`${name} joined the chat`);
+        console.log("User Name Of New Joined user Is :", name);
         socket.broadcast.emit('user-joined', name);
     });
 
     socket.on('send', message => {
         socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
     });
-
-
 
 
 });
--- js/client.js
+++ js/client.js
@@ -1,43 +1,13 @@
-const socket = io('http://localhost:5000');
+const socket = io('http://localhost:8000');
 
+// DOM Elements
 const form = document.getElementById('send-message');
 const messageInput = document.getElementById('messageinput');
 const messageContainer = document.querySelector('.container');
 
-const name = prompt("Enter your name to join the chat");
-socket.emit('new-user-joined', this.name);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Prompt for name
+const userName = prompt("Enter your name to join the chat");
+socket.emit('new-user-joined', userName);
 
 
 
